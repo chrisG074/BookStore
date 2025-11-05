3a07b6846f13c006490e32c65da9bdf0235513e3
christian G <0355639@student.rocvantwente.nl>
2025-11-05 12:13:52 +0100

    tot en met pagina 67

--- BookStore/BookStoreWeb/Controllers/CategoryController.cs
+++ BookStore/BookStoreWeb/Controllers/CategoryController.cs
@@ -6,26 +6,41 @@ namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private ApplicationDbContext _context { get; set; }
+        private readonly ApplicationDbContext _context;
 
         public CategoryController(ApplicationDbContext context)
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             return View(_context.Categories);
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Category category)
         {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Naam en Volgnummer mogen niet hetzelfde zijn");
+            }
+            if (_context.Categories.Any(c => c.Name == category.Name))
+            {
+                ModelState.AddModelError("uniquename", "Deze categorie bestaat al");
+            }
             if (ModelState.IsValid)
             {
                 _context.Categories.Add(category);
                 try
                 {
                     _context.SaveChanges();
+                    TempData["result"] = $"Categorie {category.Name} succesvol toegevoegd.";
                 }
                 catch (Exception ex)
                 {
@@ -35,6 +50,74 @@ namespace BookStoreWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(category);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Naam en Volgnummer mogen niet hetzelfde zijn");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                try
+                {
+                    _context.SaveChanges();
+                    TempData["result"] = $"Categorie {category.Name} succesvol gewijzigd.";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error = "Er is een probleem met de database!";
+                    return View(category);
+                }
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(Category category)
+        {
+            _context.Categories.Remove(category);
+            try
+            {
+                _context.SaveChanges();
+                TempData["result"] = $"Categorie {category.Name} succesvol verweiderd.";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Er is een probleem met de database!";
+                return View(category);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- BookStore/BookStoreWeb/Models/Category.cs
+++ BookStore/BookStoreWeb/Models/Category.cs
@@ -5,11 +5,12 @@ namespace BookStoreWeb.Models
     {
         [Key]
         public int? Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "{0} is een verplicht veld!")]
         [MaxLength(50, ErrorMessage = "{0} mag maximaal {1} tekens bevatten")]
         [Display(Name = "Naam")]
         public string? Name { get; set; }
         [Display(Name = "Volgnummer")]
+        [Range(minimum: 1, maximum: 100, ErrorMessage = "{0} moet tussen {1} en {2} liggen")]
         public int? DisplayOrder { get; set; }
         [Display(Name = "Aanmaak datum")]
         public DateTime? CreatedDateTime { get; set; } = DateTime.Now;
